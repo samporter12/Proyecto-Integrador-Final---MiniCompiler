a5c27875a1c33f392ab1a386eceb422bd87f8265
Samuel-Guerrero <133185229+samporter12@users.noreply.github.com>
2025-11-10 06:25:57 +0000

    cambios realizados segun las especificaciones del profe

--- output_flujo_1.py
+++ output_flujo_1.py
@@ -2,32 +2,29 @@
 
 # --- Declaración de Variables Globales ---
 estado = None
-estado = None
 t1 = None
 
 # --- Definición de Tareas (Funciones) ---
 
 def inicio():
-    global estado
     global t1
+    global estado
     print("Comenzando Flujo 1")
     estado = "OK"
     t1 = estado == "OK"
     if t1:
         fin()
-        fin()
     # Etiqueta L1: (manejada por indentación)
     return
 
 def fin():
-    global estado
     global t1
+    global estado
     print("Terminado Flujo 1")
     return
 
 # --- Punto de Entrada ---
 if __name__ == '__main__':
     print('--- Iniciando Flujo de Trabajo ---')
-    inicio()
     inicio()
     print('--- Flujo de Trabajo Terminado ---')
